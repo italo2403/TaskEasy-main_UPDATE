7a6f3d66812a1c35d03e746441acbd44e7db224f
Ítalo Nunes <37880520+italo2403@users.noreply.github.com>
2023-09-28 18:33:39 -0300

    Update banco.sql

--- TaskEasy-main/banco.sql
+++ TaskEasy-main/banco.sql
@@ -7,3 +7,11 @@ nome_completo varchar(300),
 email varchar(300),
 senha  varchar(300)
 ); 
+
+CREATE TABLE tarefas (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    descricao VARCHAR(255) NOT NULL
+);
+
+select * from cadastros;
+select * from tarefas;
